1733c4a269689f54c9d6fbe5ca66166ffcd9c453
NguyenViet25 <vietnqhe172363@fpt.edu.vn>
2025-06-20 22:32:58 +0700

    update

---
+++ ClinicManagementSystemMaven/src/main/java/model/Patient.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.sql.Date;
+
+public class Patient {
+    private int id;
+    private String fullName;
+    private Date dob;
+    private String gender;
+    private String phone;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
